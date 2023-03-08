a46d8508fb21f7ca3f7f2e518092204c8fc82ef1
ARN1380 <alireza.naghavi0@gmail.com>
2023-03-08 10:39:24 +0330

    toast added to addCard component

--- src/components/NoteApp/AddCard/AddCard.jsx
+++ src/components/NoteApp/AddCard/AddCard.jsx
@@ -1,16 +1,25 @@
 import React, { useRef, useState } from 'react';
 import './style.css'
 import { v4 as uuidv4 } from 'uuid';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function AddCard({ addHandler }) {
     const textarea = useRef()
-    
+
     const [remainingCharacter, setRemainingCharacter] = useState(200)
     const handleClick = () => {
         const id = uuidv4();
         console.log(`add card id: ${id}`);
         let textvalue = textarea.current.value
-        textvalue.length != 0 && addHandler(textvalue,id);
+        // textvalue.length != 0 ? addHandler(textvalue,id):
+        if (textvalue.length != 0) {
+            addHandler(textvalue, id)
+        } else {
+            toast.warn("nothing in card!");
+
+            console.log("toast called");
+        }
         textarea.current.value = "";
         setRemainingCharacter(200);
     }
@@ -28,6 +37,18 @@ function AddCard({ addHandler }) {
                 <span className='charachter-remaining'>{remainingCharacter} char</span>
                 <button className='add-btn' onClick={handleClick}>Add</button>
             </div>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
         </div>
     );
 }
